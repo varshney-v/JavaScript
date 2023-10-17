2ec31e64b3aa329f35f0e12d5cc9501fadfb8a75
Vipul Varshney <vipulvarshney1@gmail.com>
2023-10-17 15:15:09 +0530

    Cart page and other assignments

---
+++ Basic_Java_Script/Drop_Down_assignment/Drop_Down_assignments.js
@@ -0,0 +1,15 @@
+
+const date=document.getElementById("date")
+function date2() {
+    for(i=1;i<=31;i++)
+		{
+			if(i<10)
+			{
+				date.innerHTML+= document.innerHTML(i);
+			}
+			else
+			{
+				document.getElementById("date").innerHTML= document.innerHTML(i);
+			}
+		}
+	};
